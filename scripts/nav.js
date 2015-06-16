070c6f8ca21b42d5d4acd68c25c68af433efd143
Mike Sinkula <mike@mikesinkula.com>
2015-06-16 14:28:05 -0700

    $(this).stick(#fork).inIt();

--- scripts/nav.js
+++ scripts/nav.js
@@ -8,43 +8,43 @@ Author URI: http://www.premiumdw.com/
 
 $(window).load(function() { // when the window loads...
 	
-	var $ = jQuery;
-	var loadWidth = $(window).width();
+	var $ = jQuery; // prevent jQuery conflicts
+	var loadWidth = $(window).width(); // save window load width as a variable
 	
 	var mainToggle = function() {
 		
-		$("#nav-items").slideToggle(); // toggle main menu
+		$("#nav-items").slideToggle(); // enable main-menu toggle
 
 	};
 	
-	$("#nav-title").on("click", mainToggle); 
+	$("#nav-title").on("click", mainToggle); // trigger main-menu toggle
 	
-	var subToggle = function() {
+	var subToggle = function(e) {
 		
-		$(this).next("#nav-items li > ul.sub-menu").slideToggle(); // toggle sub-menu
-		return false; // disable href on main menu item
+		$(this).next("#nav-items li > ul.sub-menu").slideToggle(); // enable sub-menu toggle
+		e.preventDefault(); // disable href on main-menu item
 		
 	};
 	
-	if ($(window).width() < 801) {
+	if ($(window).width() < 801) { // if width is less than 801px...
 		
-		$("#nav-items li.main > a").on("click", subToggle);
+		$("#nav-items li.main > a").on("click", subToggle); // trigger sub-menu toggle 
 	
 	}	
 	
 	$(window).resize(function() { // when the window is resized...
 			
-		if ( loadWidth != $(window).width() ) { // disable vertical resize
+		if ( loadWidth != $(window).width() ) { // disable vertical resize!
 			
 			if ($(window).width() < 801) { // if width is less than 801px...
 			
 				$("#nav-items").hide(); // hide the navigation items
-				$("#nav-items li.current-page-item ul.sub-menu").show(); // show current sub-menu
-				$("#nav-items li.current-page-parent ul.sub-menu").show(); // show current sub-menu
+				$("#nav-items li.current-page-item ul.sub-menu").show(); // show current page item sub-menu
+				$("#nav-items li.current-page-parent ul.sub-menu").show(); // show current page parent sub-menu
 				
-				if ($("#nav-items li.main > a").unbind( "click", subToggle)) { // if click has been previously unbound...
+				if ($("#nav-items li.main > a").unbind( "click", subToggle)) { // if sub-menu toggle trigger click has been previously unbound...
 				
-					$("#nav-items li.main > a").bind( "click", subToggle); // ... bind it again once and only once
+					$("#nav-items li.main > a").bind( "click", subToggle); // ... bind it again (once and only once) to trigger sub-menu toggle click
 						
 				}
 									
@@ -52,7 +52,7 @@ $(window).load(function() { // when the window loads...
 			
 				$("#nav-items").show(); // show the navigation items
 				$("#nav-items li > ul.sub-menu").hide();// close sub-menu
-				$("#nav-items li.main > a").unbind( "click", subToggle);
+				$("#nav-items li.main > a").unbind( "click", subToggle); // unbind sub-menu toggle trigger click
 				
 			}
 			
