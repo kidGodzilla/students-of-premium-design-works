dafc48631bdbe46abb682ce846986d75ea7ee6a8
Mike Sinkula <mike@mikesinkula.com>
2015-06-21 13:57:14 -0700

    O.C.D.

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,7 +1,5 @@
 /*  
 Theme Name: Students of Premium Design Works
-Description: This is a theme your mom would finally respond to.
-Version: 3
 Author: Premium Design Works
 Author URI: http://www.premiumdw.com/
 */
@@ -42,20 +40,19 @@ $(window).load(function() { // when the window loads...
 	$("#nav-items li.main > a").on("click", classToggle); // trigger sub-menu icon toggle
 	$("#nav-items li.current-page-item > a").addClass("opened"); // open toggle icon on current page
 	$("#nav-items li.current-page-parent > a").addClass("opened"); // open toggle icon on current page parent
+
 	
 	var loadWidth = $(window).width(); // save window load width as a variable
 
 	$(window).resize(function() { // when the window is resized...
 			
-		if ( loadWidth !== $(window).width() ) { // only horizontally...
+		if ( loadWidth !== $(window).width() ) { // disable vertical resize!
 			
 			if ($(window).width() < 801) { // if width is less than 801px...
 			
 				$("#nav-items").hide(); // hide the navigation items
 				$("#nav-items li.current-page-item ul.sub-menu").show(); // show current page item sub-menu
-				$("#nav-items li.current-page-item > a").addClass("opened"); // open toggle icon on current page
 				$("#nav-items li.current-page-parent ul.sub-menu").show(); // show current page parent sub-menu
-				$("#nav-items li.current-page-parent > a").addClass("opened"); // open toggle icon on current page parent
 				
 				if ($("#nav-items li.main > a").unbind( "click", subToggle)) { // if sub-menu toggle trigger click has been previously unbound...
 				
@@ -63,7 +60,7 @@ $(window).load(function() { // when the window loads...
 						
 				}
 									
-			} else { // else...
+			} else if ($(window).width() > 800) { // else if width is greater than 800px...
 			
 				$("#nav-items").show(); // show the navigation items
 				$("#nav-items li > ul.sub-menu").hide();// close sub-menu
@@ -71,7 +68,7 @@ $(window).load(function() { // when the window loads...
 				
 			}
 			
-		} // end only horizontally
+		} // end disable verticle resize
 		
 	}); // end window resize
 		
